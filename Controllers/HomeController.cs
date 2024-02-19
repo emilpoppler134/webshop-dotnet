0a1e23decb6e70b50dde1d81382e3c3777dc5f87
emilpoppler134 <emil@50archive.com>
2024-02-19 11:29:35 +0100

    Add the checkout page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -64,7 +64,44 @@ namespace Webshop.Controllers
 
         public async Task<IActionResult> Checkout()
         {
-            return View();
+            string? cookies = Request.Cookies["cart"];
+            List<int> cart = new List<int>();
+
+            try
+            {
+                if (cookies != null)
+                {
+                    string cartString = Uri.UnescapeDataString(cookies);
+                    List<int>? convertedCart = JsonConvert.DeserializeObject<List<int>>(cartString);
+                    
+                    if (convertedCart != null)
+                    {
+                        cart = convertedCart;
+                    }
+                }
+            }
+            catch (JsonReaderException ex)
+            {
+                return BadRequest("Invalid format in cart cookie: " + ex.Message);
+            }
+
+            var response = await (
+                from stock in _context.Stocks
+                join product in _context.Products on stock.ProductId equals product.Id
+                where cart.Contains(stock.Id)
+                select new StockExtended
+                {
+                    Id = stock.Id,
+                    Price = stock.Price,
+                    Size = stock.Size,
+                    Quantity = stock.Quantity,
+                    Product = _context.Products.Where(s => s.Id == stock.ProductId).First()
+                }
+            ).ToListAsync();
+
+            List<StockExtended> extendedStockList = response;
+
+            return View(extendedStockList);
         }
     }
 }
